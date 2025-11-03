fab1bf1c73c6d448a7fee114e1e6c401ee49ef87
Dhruva Kamble <dhruvakamble@Mac-1305.lan>
2025-11-03 13:11:52 -0500

    port changed to 5000

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("/api/admin", verifyToken, allowRoles("admin"), (req, res) => {
   res.send("Welcome Admin!");
 });
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
